1bcc2d6180023e952b2bd6428f32f03c26d45183
Andrew Alteri <andrewsalteri@gmail.com>
2020-10-20 17:37:55 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    manipulate();
+  });
+
+  function manipulate() {
+      document.getElementById("text");
+      text.textContent = ("This is really cool!");
+  }
+
+  
