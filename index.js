2c9aa8226490b39fd07a433ce5e49825a67ea932
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-11-07 21:38:58 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+// DOMContentLoaded event 
+document.addEventListener("DOMContentLoaded", function() {
+    let text = document.getElementById('text').innerHTML = "This is really cool!"
+  });
+
+
+  
